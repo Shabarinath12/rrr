5c336f98688449d942c9bcd94f0d60e68b6cad72
Shabrinath Reddy <shabrinathreddy@Shabrinaths-MacBook-Air.local>
2019-12-20 19:22:47 +0530

    commit for landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomepageComponent } from './pages/welcomepage/welcomepage.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {LayoutModule} from "@angular/cdk/layout";
+import { BannerComponent } from './components/banner/banner.component';
+import { BodyComponent } from './components/body/body.component';
+import { ContentComponent } from './components/content/content.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomepageComponent,
+    NavbarComponent,
+    BannerComponent,
+    BodyComponent,
+    ContentComponent,
+    ProfileComponent,
+    SignupComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
